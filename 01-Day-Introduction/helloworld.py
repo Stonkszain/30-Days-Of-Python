8e1bb995ae8b05c34bc19ac171364e1a6b0e0851
Stonkszain <z.z.safdar2@ncl.ac.uk>
2023-08-07 11:50:35 +0100

    feat: add data type checking
    All python data types checked using the type() function

--- 01-Day-Introduction/helloworld.py
+++ 01-Day-Introduction/helloworld.py
@@ -5,3 +5,22 @@ print(3 % 2)
 print(4 / 2)
 print(2 ** 3)
 print(3 // 2)
+
+print('zain')
+print('UK')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('zain'))
+print(type("UK"))
+
+print(type(True))
+print(type(('Xbox', 499.99)))
+print(type({1,2,3}))
+print(type({"harry" : "styles"}))
+
+print("The Euclidian distance between (2, 3) and (10, 8) is", ((2 - 10) ** 2 + (3 - 8) ** 2) ** 0.5)
