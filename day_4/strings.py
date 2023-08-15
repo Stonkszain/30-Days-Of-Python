b780caf196393d2de02a29639a1d9e1a752d60dc
Stonkszain <z.z.safdar2@ncl.ac.uk>
2023-08-15 21:40:25 +0100

    feat: completed day 4

---
+++ day_4/strings.py
@@ -0,0 +1,81 @@
+
+# print('Thirty' + ' ' + 'Days' + ' ' + 'Of' + ' ' + 'Python')
+
+# print('Coding' + ' ' + 'For' + ' ' + 'All')
+
+company = 'Coding' + ' ' + 'For' + ' ' + 'All'
+
+# print(company)
+# print(len(company))
+# print(company.upper())
+# print(company.lower())
+# print(company.capitalize())
+# print(company.title())
+# print(company.swapcase())
+
+# print(company.replace('Coding ', ''))
+
+# print(company.find('Coding'))
+
+# print(company.replace('Coding', 'Python'))
+
+# print('Python for Everyone'.replace('Everyone', 'All'))
+
+# print(company.split())
+
+# print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(', '))
+
+# print(company[0])
+
+# print(company[-1])
+
+# print(company[10])
+
+# acronym = ''
+
+# for i in "Python For Everyone":
+#     if i.isupper():
+#         acronym += i
+    
+# print(acronym)
+
+# print('Coding For All'.index('C'))
+
+# print('Coding For All'.index('F'))
+
+# print('Coding For All People'.rfind('l'))
+
+# print('You cannot end a sentence with because because because is a conjunction'.index('because'))
+
+# print('You cannot end a sentence with because because because is a conjunction'.rfind('because'))
+
+# because_string = 'You cannot end a sentence with because because because is a conjunction'
+
+# print(because_string[because_string.find('because'):because_string.find('because') + len('because because because')])
+
+# print('Coding For All'.startswith('Coding'))
+
+# print('Coding For All'.endswith('coding'))
+
+# print('  Coding For All     '.strip())
+
+# print('30DaysOfPython'.isidentifier())
+# print('thirty_days_of_python'.isidentifier())
+
+# print('# '.join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
+
+# print("I am enjoying this challenge.\nI just wonder what is next")
+
+# print('Name\tAge\tCountry\tCity\nAsabeneh\t250\tFinland\tHelsinki')
+
+# radius = 10
+# area = 3.14 * radius ** 2
+# print('The area of a circle with radius {} is {} meters square.'.format(radius, int(area)))
+
+print(f'8 + 6 = {8 + 6}\n' + 
+      f'8 - 6 = {8 - 6}\n' + 
+      f'8 * 6 = {8 * 6}\n' +
+      f'8 / 6 = {8 / 6}\n' +
+      f'8 % 6 = {8 % 6}\n' +
+      f'8 // 6 = {8 // 6}\n' +
+      f'8 ** 6 = {8 ** 6}\n')
